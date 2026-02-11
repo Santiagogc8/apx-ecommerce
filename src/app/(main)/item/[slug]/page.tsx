1368f43bec55d7de4a44e2e8d897c2f71047af19
Santiago Guzman <guzmansantiago833@gmail.com>
2026-02-10 23:39:52 -0500

    add ProductGallery

--- src/app/(main)/item/[slug]/page.tsx
+++ src/app/(main)/item/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { fetchApi } from "@/src/lib/api";
 import Link from "next/link";
 import { PrimaryBtn } from "@/src/ui/PrimaryBtn";
+import { ProductGallery } from "@/src/ui/ProductGallery";
 
 const priceFormatter = new Intl.NumberFormat('es-ES', {
     minimumFractionDigits: 0,
@@ -14,16 +15,16 @@ export default async function ItemPage({params,}: {params: Promise<{ slug: strin
     const stock = product.stock <= 0;
 
 	return (
-		<div className="py-30 flex flex-col items-center justify-center w-full">
-			<div className="w-[80%] flex justify-center gap-10">
-                <div className="bg-orange-100">
-                    Producto
+		<div className="py-30 flex flex-col items-center justify-center w-full xl:pt-30 xl:pb-0">
+			<div className="w-[80%] flex flex-col justify-center gap-10 xl:flex-row">
+                <div className="min-w-[60%]">
+                    <ProductGallery gallery={product.images}/>
                 </div>
-                <div>
-                    <h3>{product.name}</h3>
+                <div className="flex flex-col gap-5">
+                    <h3 className="text-5xl">{product.name}</h3>
                     <p>{product.description}</p>
                     {stock && <p className="text-red-500 font-bold">Actualmente no hay stock para este producto</p>}
-                    <p className="text-orange-500">${priceFormatter.format(+product.unit_cost)}</p>
+                    <p className="text-orange-500 text-xl">${priceFormatter.format(+product.unit_cost)}</p>
                     <Link href={'#'}>
                         <PrimaryBtn>Comprar</PrimaryBtn>
                     </Link>
--- src/ui/ProductGallery.tsx
+++ src/ui/ProductGallery.tsx
@@ -1,19 +1,101 @@
-"use client"
-import { useState } from "react"
+"use client";
+import { useState, useRef, useEffect } from "react";
 
-export function ProductGallery({gallery}: {gallery: Array<string>}){
-    const [currentImage, setCurrentImage] = useState(0);
+export function ProductGallery({ gallery }: { gallery: Array<string> }) {
+	const [currentImage, setCurrentImage] = useState(0);
+	const thumbnailsDiv = useRef(null);
+	const [canScrollLeft, setCanScrollLeft] = useState(false);
+	const [canScrollRight, setCanScrollRight] = useState(true);
+	const [isZoomed, setIsZoomed] = useState(false);
 
-    return (
-        <div>
-            <div>
-                <img src={gallery[currentImage]} alt="imagen principal" />
-            </div>
-            {gallery.length > 1 && (
-                <div>
-                    {gallery.map((image, index) => <img src={image} alt="imagen" onClick={()=> setCurrentImage(index)} key={index}/>)}
-                </div>
-            )}
-        </div>
-    )
+	const checkScroll = () => {
+		if (thumbnailsDiv.current) {
+			const { scrollLeft, scrollWidth, clientWidth } = thumbnailsDiv.current;
+
+			setCanScrollLeft(scrollLeft > 0);
+
+			setCanScrollRight(Math.ceil(scrollLeft + clientWidth) < scrollWidth);
+		}
+	};
+
+	useEffect(() => {
+		checkScroll();
+		window.addEventListener("resize", checkScroll);
+		return () => window.removeEventListener("resize", checkScroll);
+	}, [gallery]);
+
+	useEffect(() => {
+		if (isZoomed) {
+			document.body.style.overflow = "hidden";
+		} else {
+			document.body.style.overflow = "unset";
+		}
+		return () => {
+			document.body.style.overflow = "unset";
+		};
+	}, [isZoomed]);
+
+	const handleLeftClick = () => {
+		thumbnailsDiv.current.scrollBy({ left: -150, behavior: "smooth" });
+	};
+
+	const handleRightClick = () => {
+		thumbnailsDiv.current.scrollBy({ left: 150, behavior: "smooth" });
+	};
+
+	return (
+		<div className="flex flex-col gap-5 w-full">
+			{isZoomed && (
+				<div
+					onClick={() => setIsZoomed(false)}
+					className="fixed inset-0 bg-black/40 backdrop-blur-md z-20 transition-opacity duration-300 animate-in fade-in"
+				/>
+			)}
+			<div className="bg-orange-100 p-4 rounded-xl min-h-65 flex justify-center items-center md:min-h-110">
+				<img
+					onClick={() => setIsZoomed(!isZoomed)}
+					src={gallery[currentImage]}
+					className={`cursor-zoom-in object-contain ${isZoomed ? "fixed inset-0 m-auto z-30 cursor-zoom-out w-auto md:h-[70vh] object-contain max-w-[90%]" : "scale-100"}`}
+					alt="thumbnail main"
+				/>
+			</div>
+			{gallery.length > 1 && (
+				<div className="relative flex items-center group">
+					{canScrollLeft && (
+						<button
+							onClick={handleLeftClick}
+							className="bg-gray-900/60 hover:bg-gray-900/80 transition-colors h-full px-3 text-white absolute left-0 z-10 cursor-pointer"
+						>
+							{"<"}
+						</button>
+					)}
+
+					<div
+						ref={thumbnailsDiv}
+						onScroll={checkScroll}
+						className="flex gap-3 overflow-x-auto relative w-full snap-x"
+					>
+						{gallery.map((image, index) => (
+							<img
+								key={index}
+								src={image}
+								alt={`thumbnail ${index}`}
+								onClick={() => setCurrentImage(index)}
+								className={`bg-orange-100 p-2 rounded-md w-24 h-24 object-contain cursor-pointer shrink-0 snap-start transition-all ${index === currentImage ? "border-2 border-orange-500" : ""}`}
+							/>
+						))}
+					</div>
+
+					{canScrollRight && (
+						<button
+							onClick={handleRightClick}
+							className="bg-gray-900/60 hover:bg-gray-900/80 transition-colors h-full px-3 text-white absolute right-0 z-10 cursor-pointer"
+						>
+							{">"}
+						</button>
+					)}
+				</div>
+			)}
+		</div>
+	);
 }
